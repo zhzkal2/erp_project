f9c9233ef66b9dbe99898cb4b9fddf35daaf0241
zhzkal2 <reliya1541@gmail.com>
2024-12-20 20:01:47 +0900

    feat : erp 프로젝트 최초커밋

---
+++ src/main/java/com/java/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.java.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
