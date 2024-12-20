2cc5b5b642210af9dc7ad1cf96bb93fa5783f533
zhzkal2 <reliya1541@gmail.com>
2024-12-20 21:45:30 +0900

    feat : postgre 환경설정 추가

--- src/main/java/com/java/Main.java
+++ src/main/java/com/java/Main.java
@@ -2,8 +2,10 @@ package com.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Main {
 
     public static void main(String[] args) {
